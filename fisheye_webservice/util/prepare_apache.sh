d02c7a12884aebcdbd594377bf601d663a3d3584
Dmitry Belous <dmigous@gmail.com>
2016-10-28 23:48:54 +0300

    added make all at util/prepare_apache.sh

--- fisheye_webservice/util/prepare_apache.sh
+++ fisheye_webservice/util/prepare_apache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 ln -s /mnt/fisheye/fisheye_webservice/ /var/www/html/
 chown apache:apache /var/www/html/fisheye_webservice/ -R
@@ -6,5 +7,5 @@ chown apache:apache /var/www/html/fisheye_webservice/ -R
 cat /var/www/html/fisheye_webservice/httpd_fisheye_webservice.conf >> /etc/httpd/conf/httpd.conf
 pip3 install -r /var/www/html/fisheye_webservice/requirements.txt
 
-cd /mnt/fisheye/fisheye && make install
+cd /mnt/fisheye/fisheye && make all install
 cd /mnt/fisheye/fisheye/python/ && python setup.py install
