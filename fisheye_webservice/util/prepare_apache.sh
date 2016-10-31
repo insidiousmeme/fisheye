06a649d9ab214587fdd9bac180b0fa8735dde11a
Dmitry Belous <dmigous@gmail.com>
2016-10-31 15:14:28 +0200

    prepare_apache.sh fixed chown line

--- fisheye_webservice/util/prepare_apache.sh
+++ fisheye_webservice/util/prepare_apache.sh
@@ -2,7 +2,7 @@
 set -e
 
 ln -s /mnt/fisheye_webservice/ /var/www/html/
-chown apache:apache /var/www/html/fisheye_webservice/ -R
+chown apache:apache /var/www/html/fisheye_webservice -R
 
 cat /var/www/html/fisheye_webservice/httpd_fisheye_webservice.conf >> /etc/httpd/conf/httpd.conf
 pip3 install -r /var/www/html/fisheye_webservice/requirements.txt
