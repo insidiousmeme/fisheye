cce002b6385cae8637d15c005cbad7df8e3153f0
Dmitry Belous <dmigous@gmail.com>
2016-10-31 15:09:49 +0200

    adjusted prepare_apache.sh to work from inside the repo folder

--- fisheye_webservice/util/prepare_apache.sh
+++ fisheye_webservice/util/prepare_apache.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-ln -s /mnt/fisheye/fisheye_webservice/ /var/www/html/
+ln -s /mnt/fisheye_webservice/ /var/www/html/
 chown apache:apache /var/www/html/fisheye_webservice/ -R
 
 cat /var/www/html/fisheye_webservice/httpd_fisheye_webservice.conf >> /etc/httpd/conf/httpd.conf
 pip3 install -r /var/www/html/fisheye_webservice/requirements.txt
 
-cd /mnt/fisheye/fisheye && make all install
-cd /mnt/fisheye/fisheye/python/ && python setup.py install
+cd /mnt/fisheye && make all install
+cd /mnt/fisheye/python/ && python setup.py install
