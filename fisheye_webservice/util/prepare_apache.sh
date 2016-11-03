91d91b9ed023d8dc83f41d037c688391f4d3a3cb
Dmitry Belous <dmigous@gmail.com>
2016-11-03 16:13:34 +0200

    util/prepare_apache.h: added check before writing to httpd.conf to prevent several cases when added several times

--- fisheye_webservice/util/prepare_apache.sh
+++ fisheye_webservice/util/prepare_apache.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-ln -s /mnt/fisheye_webservice/ /var/www/html/
+ln -sf /mnt/fisheye_webservice/ /var/www/html/
 chown apache:apache /var/www/html/fisheye_webservice -R
 chown apache:apache /mnt/fisheye_webservice -R
 
-cat /var/www/html/fisheye_webservice/httpd_fisheye_webservice.conf >> /etc/httpd/conf/httpd.conf
+if ! grep -q "fisheye_webservice" /etc/httpd/conf/httpd.conf; then
+  cat /var/www/html/fisheye_webservice/httpd_fisheye_webservice.conf >> /etc/httpd/conf/httpd.conf
+fi
 pip3 install -r /var/www/html/fisheye_webservice/requirements.txt
 
 cd /mnt/fisheye && make all install
