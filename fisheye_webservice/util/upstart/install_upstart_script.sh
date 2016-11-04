f2b6f399296721c4adbf534fe043083821283d97
Dmitry Belous <dmigous@gmail.com>
2016-11-04 12:32:57 +0200

    upstart files

---
+++ fisheye_webservice/util/upstart/install_upstart_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_ROOT=$ROOT_DIR/../../../
+UPSTART_TEMPLATE=$ROOT_DIR/fisheye_webserver_template.conf
+UPSTART_OUTPUT_CONF=$ROOT_DIR/fisheye_webserver.conf
+# UPSTART_OUTPUT_CONF=/etc/init/fisheye_webserver.conf
+sed "s@__fisheye_repo_path__@$REPO_ROOT@g" $UPSTART_TEMPLATE > $UPSTART_OUTPUT_CONF
